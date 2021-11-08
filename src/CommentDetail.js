e14fa0b465455bf84a38eb6370173a7ef522e54f
Nick Vitulli <nvitulli71@live.com>
2021-11-08 11:00:23 -0500

    working with props and nesting components

--- src/CommentDetail.js
+++ src/CommentDetail.js
@@ -1,23 +1,22 @@
-import React from 'react';
-import faker from "faker";
+import React from "react";
 
-const CommonDetail = () => {
-    return (
-        <div className="comment">
-            <a href="/" className="avatar">
-                <img alt="avatart" src={faker.image.avatar()} />
-            </a>
-            <div className="content">
-                <a href="/" className="author">
-                    Nick
-                </a>
-                <div className="metaData">
-                    <span className="date">Today at 6:00 PM</span>
-                </div>
-                <div className="text">Big Bad blog post</div>
-            </div>
+const CommonDetail = (props) => {
+  return (
+    <div className="comment">
+      <a href="/" className="avatar">
+        <img alt="avatar" src={props.avatar} />
+      </a>
+      <div className="content">
+        <a href="/" className="author">
+          {props.author}
+        </a>
+        <div className="metaData">
+          <span className="date">{props.timeAgo}</span>
         </div>
-    );
-}
+        <div className="text">{props.blogPost}</div>
+      </div>
+    </div>
+  );
+};
 
 export default CommonDetail;
--- src/index.js
+++ src/index.js
@@ -1,13 +1,32 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import faker from "faker";
+
 import CommentDetail from "./CommentDetail";
+import ApprovalCard from "./ApprovalCard";
 
 const App = () => {
-    return (
-        <div className="ui container comments">
-            <CommentDetail />
-        </div>
-    );
-}
+  return (
+    <div className="ui container comments">
+      <ApprovalCard>
+        <CommentDetail
+          author="Nick"
+          timeAgo="Today at 10:00 AM"
+          avatar={faker.image.avatar()}
+          blogPost="HELLO!!!!"
+        />
+      </ApprovalCard>
 
-ReactDOM.render(<App />, document.querySelector('#root'));
+      <ApprovalCard>
+        <CommentDetail
+          author="John"
+          timeAgo="Yesterday at 3:57 PM"
+          avatar={faker.image.avatar()}
+          blogPost="Good bye!"
+        />
+      </ApprovalCard>
+    </div>
+  );
+};
+
+ReactDOM.render(<App />, document.querySelector("#root"));
