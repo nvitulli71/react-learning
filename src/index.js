3f69ed07b78259c6a5b6f4c52d9f86672f6fbaf6
Nick Vitulli <nvitulli71@live.com>
2021-11-08 12:54:10 -0500

    learning the difference between class and function based components

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
 import React from "react";
 import ReactDom from "react-dom";
 
-const App = () => {
-  window.navigator.geolocation.getCurrentPosition(
-    (position) => console.log(position),
-    (error) => console.error(error)
-  );
-  return <div>Latitude: </div>;
-};
+class App extends React.Component {
+  render() {
+    window.navigator.geolocation.getCurrentPosition(
+      (position) => console.log(position),
+      (error) => console.error(error)
+    );
+
+    return <div>Latitude: </div>;
+  }
+}
 
 ReactDom.render(<App />, document.querySelector("#root"));
