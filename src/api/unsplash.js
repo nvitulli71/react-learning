4dccd607c329c12e2f8d69ff42f359c315d4cc4a
Nick Vitulli <nvitulli71@live.com>
2021-11-19 14:19:52 -0500

    utilizing axios to do API calls

---
+++ src/api/unsplash.js
@@ -0,0 +1,8 @@
+import axios from "axios";
+
+export default axios.create({
+  baseUrl: "https://api.unsplash.com/",
+  headers: {
+    Authorization: "Client-ID PaPNMWEhJIm9lytAGPsOIx8Xb6fX6nGS0E9kI8fY1LI",
+  },
+});
--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,29 @@
 import React from "react";
+
+import unsplash from "../api/unsplash";
 import SearchBar from "./SearchBar";
 
-const App = () => {
-  return (
-    <div className="ui container" style={{ marginTop: "10px" }}>
-      <SearchBar />
-    </div>
-  );
-};
+class App extends React.Component {
+  state = { images: [] };
+
+  onSearchSubmit = async (term) => {
+    const resposnse = await unsplash.get("/search/photos", {
+      params: {
+        query: term,
+      },
+    });
+
+    this.setState({ images: resposnse.data.results });
+  };
+
+  render() {
+    return (
+      <div className="ui container" style={{ marginTop: "10px" }}>
+        <SearchBar onSubmit={this.onSearchSubmit} />
+        Found: {this.state.images.length} images...
+      </div>
+    );
+  }
+}
 
 export default App;
